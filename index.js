137c0b0c22a81dfb59a6f5ba7173af01331b2eee
Krishan Rathore <krishan.rathore@hcode.tech>
2026-04-08 15:57:00 +0530

    Set up Express server and add .gitignore; include package.json for dependencies

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
-const http = require('http');
+/** -------------- Http Server -------------- */
+// const http = require('http');
 
-const server = http.createServer((req, res) => {
-  res.end('Hello from Node.js 🚀');
+// const server = http.createServer((req, res) => {
+//   res.end('Hello from Node.js 🚀');
+// });
+
+// server.listen(3000, () => {
+//   console.log('Server running on http://localhost:3000');
+// });
+
+/** -------------- EXPRESS -------------- */
+
+const express = require('express');
+const app = express();
+const PORT = 3000;
+app.use(express.json()); // Parse JSON bodies (like fetch/axios POST data)
+// GET route — like a Next.js API route GET handler
+app.get('/', (req, res) => {
+  res.json({ message: 'Hello from Node.js!' });
 });
-
-server.listen(3000, () => {
-  console.log('Server running on http://localhost:3000');
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
 });
